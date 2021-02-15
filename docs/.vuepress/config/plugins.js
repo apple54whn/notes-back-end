6b3ea8765fc2e507fcbc935b8b08f60a01dd1451
conanan <54whn54@gmail.com>
2021-02-16 01:44:33 +0800

    mermaid

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -43,7 +43,10 @@ module.exports = [
     'one-click-copy',
     {
       // 代码块复制按钮
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copySelector: [
+        'div[class*="language-"] pre',
+        'div[class*="aside-code"] aside',
+      ], // String or Array
       copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
       duration: 1000, // prompt message display time.
       showInMobile: false, // whether to display on the mobile side, default: false.
@@ -107,4 +110,6 @@ module.exports = [
       },
     },
   ],
+  // mermaid 图
+  'vuepress-plugin-mermaidjs',
 ]
